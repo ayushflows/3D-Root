204cf9a95aa645af6bdc122526af488a62b794d2
Ayush Tripathi <ayushflows@gmail.com>
2025-03-10 09:16:54 +0530

    added api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/auth",
+    baseURL: "https://threed-root.onrender.com/auth",
     // withCredentials: true,
 });
 
